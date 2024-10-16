215fc79df1380053c0ec891312d8f71d3cc12fcc
Martin Jonas <martinjonas@gmail.com>
2024-10-16 11:42:12 -0300

    delete docker files as it's converted in submodule

--- docker-riscv32/compliance/build.sh
+++
@@ -1,3 +0,0 @@
-$CC -static -march=rv32g -mabi=ilp32 -nostdlib -nostartfiles -Wl,--no-warn-rwx-segments -I./ -I./../../ -I/riscv-tests/isa/macros/scalar/ -T./../linker/link.ld -o build/$1.elf /riscv-tests/isa/rv32ui/$1.S
-$QEMU -d in_asm -D build/$1.s build/$1.elf
-$DUMP -d build/$1.elf > build/$1_dump.s
--- docker-riscv32/docker-build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t riscv32:latest  riscv32 
-docker build -t compliance:latest compliance
-docker build -t verifier:latest verifier
