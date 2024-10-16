038976aff11690d656faba42d8d7b0fd35260e47
Martin Jonas <martinjonas@gmail.com>
2024-10-16 10:57:20 -0300

    small fixes

--- docker-riscv32/compliance/build_all.sh
+++ docker-riscv32/compliance/build_all.sh
@@ -1,6 +1,8 @@
 cd compliance
 chmod +x build.sh
 chmod +x build_mul.sh
+dos2unix build.sh
+dos2unix build_mul.sh
 for f in /riscv-tests/isa/rv32ui/*.S; do
   filename=$(basename "${f%.*}")
   if [[ "$filename" != "fence_i" ]]; then
