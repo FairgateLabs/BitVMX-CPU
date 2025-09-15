8520ca866f84b37dac95ca1d7d65b9f4113e4f57
crivasr <camilorivasramunni@gmail.com>
2025-09-15 14:08:20 -0300

    rename test

--- emulator/tests/audit.rs
+++ emulator/tests/audit.rs
@@ -13,7 +13,7 @@ fn audit_tests() {
         if let Ok(path) = path {
             let fname = path.file_name();
             let fname = fname.to_string_lossy();
-            if fname.ends_with(".elf") && !fname.contains("audit_01") {
+            if fname.ends_with("verify.elf") {
                 let path = path.path();
                 let path = path.to_string_lossy();
 
