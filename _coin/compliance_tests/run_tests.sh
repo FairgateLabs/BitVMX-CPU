0e80e3583557dad4852dbc9ba9fc82807caec596
crivasr <camilorivasramunni@gmail.com>
2025-09-26 12:03:22 -0300

    add no-mapping flag

--- _coin/compliance_tests/run_tests.sh
+++ _coin/compliance_tests/run_tests.sh
@@ -6,7 +6,12 @@ if [[ "$1" == "--verify" ]]; then
     VERIFY_FLAG="--verify"
 fi
 
+NO_MAPPING_FLAG=""
+if [[ "$2" == "--no-mapping" ]]; then
+    NO_MAPPING_FLAG="--no-mapping"
+fi
+
 for f in ./build/*.elf; do
     echo "Running test $(basename "$f")"
-    ../../target/release/emulator execute --elf "$f" $VERIFY_FLAG
+    ../../target/release/emulator execute --elf "$f" $VERIFY_FLAG $NO_MAPPING_FLAG
 done
